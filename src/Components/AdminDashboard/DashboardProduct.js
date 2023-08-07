a79100a6de26d09417df8ea2870d443ecc7fe668
“Medoelsheta1” <elsheta474@gmail.com>
2023-08-08 02:01:10 +0300

    adding_some_edits_on_Admin_Dashboard

--- src/Components/AdminDashboard/DashboardProduct.js
+++ src/Components/AdminDashboard/DashboardProduct.js
@@ -3,11 +3,15 @@ import useFetch from '../hooks/useFetch'
 import {AiTwotoneEdit} from 'react-icons/ai'
 import {AiFillDelete} from 'react-icons/ai'
 import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 const DashboardProduct = () => {
     const [numberPage , setNumberPage] = useState(1)
     let {data , isLoading } = useFetch(`products?populate=*&pagination[page]=${numberPage}&pagination[pageSize]=10`)
-    return (
+    const navigate = useNavigate()
+    return ( 
+        <>
         <div className='dash-products'>
         <table className="table">
         <thead>
@@ -27,6 +31,8 @@ const DashboardProduct = () => {
                     <td>${ele?.attributes?.price}</td>
                     <td><Link to={`${ele?.id}`}><AiTwotoneEdit className='me-1 text-primary h5' /></Link> <AiFillDelete onClick={() => {
                         axios.delete(`https://strapi-nbja.onrender.com/api/products/${ele?.id}`)
+                        toast.success('deleted Item Successfully')
+                        navigate('/admin/home')
                     }
                     } className='h5 text-danger' /></td>
                     </tr>
@@ -35,12 +41,26 @@ const DashboardProduct = () => {
         </table>
         <div className='dash-buttons d-flex justify-content-center align-items-center'>
             <button type="button" disabled={numberPage === 1 ? true : false} className="btn btn-danger me-2" onClick={() => setNumberPage(numberPage - 1)}>Prievius</button>
-            <button type="button" disabled={numberPage === 5 ? true : false} className="btn btn-primary"onClick={() => setNumberPage(numberPage + 1)}>Next</button>
+            <button type="button" disabled={numberPage === 4 ? true : false} className="btn btn-primary"onClick={() => setNumberPage(numberPage + 1)}>Next</button>
         </div>
         <div className='dash-customization'>
 
         </div>
-        </div>
+        </div>   
+        <ToastContainer
+                position="top-left"
+                autoClose={2500}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+                />     
+        </>
+
     )
 }
 
